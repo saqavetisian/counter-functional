4284dfcea62de0fd62ddbb5b29876606aac11489
Mr. Avetisyan <sargis.av2001@gmail.com>
2024-10-29 09:43:02 +0400

    changed texts colors for light mode

--- app/page.tsx
+++ app/page.tsx
@@ -23,16 +23,16 @@ export default function Home() {
           Happy to see you here! Please click on the links below to check the stage
         </p>
         <div className="grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full">
-          <Link href="/stage-a" className="rounded-md border text-green-200 border-green-200 p-4 w-full text-center hover:bg-green-200 hover:text-black">
+          <Link href="/stage-a" className="rounded-md border dark:text-green-200 text-green-600  dark:border-green-200 border-green-600  p-4 w-full text-center dark:hover:bg-green hover:bg-600  hover:text-black">
             Stage A
           </Link>
-          <Link href="/stage-b"  className="rounded-md border text-yellow-200 border-yellow-200 p-4 w-full text-center hover:bg-yellow-200 hover:text-black">
+          <Link href="/stage-b"  className="rounded-md border dark:text-yellow-200 text-yellow-600  dark:border-yellow-200 border-yellow-600  p-4 w-full text-center dark:hover:bg-yellow hover:bg-600 hover:text-black">
             Stage B
           </Link>
-          <Link href="/stage-c"  className="rounded-md border text-amber-200 border-amber-200 p-4 w-full text-center hover:bg-amber-200 hover:text-black">
+          <Link href="/stage-c"  className="rounded-md border dark:text-amber-200 text-amber-600  dark:border-amber-200 border-amber-600  p-4 w-full text-center dark:hover:bg-amber hover:bg-600 dark:hover:text-black hover:text-600 ">
             Stage C
           </Link>
-          <Link href="/stage-d"  className="rounded-md border text-red-200 border-red-200 p-4 w-full text-center hover:bg-red-200 hover:text-black">
+          <Link href="/stage-d"  className="rounded-md border dark:text-red-200 text-red-600 border-red-600  dark:border-red-200 p-4 w-full text-center dark:hover:bg-red hover:bg-600  dark:hover:text-black hover:text-600 ">
             Stage D (optional)
           </Link>
         </div>
